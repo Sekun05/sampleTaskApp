650c78fd6d874652157c23fdfdc59a7f6f3d4c8c
Sekun05 <sekun524@gmail.com>
2020-02-17 16:05:30 +0000

    タスク一覧表示機能
    * 四章までやった
    * CakePHPのときはよく必要性がわからなかったモデルについて、すこしわかってきた

--- app/Folder.php
+++ app/Folder.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Folder extends Model
 {
-    //
+    // モデルからのリレーション用
+    public function tasks()
+    {
+        return $this->hasMany('App\Task');  // 省略しなかった場合は $this->hasMany('App\Task', 'folder_id', 'id');
+    }
 }
--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -12,11 +13,19 @@ class TaskController extends Controller
     {
         // return "Hello World!";   // for Debug
 
+        // 全フォルダを取得
         $folders = Folder::all();
+
+        // 選ばれたフォルダを取得
+        $current_folder = Folder::find($id);
+
+        // current_folder からタスクを取得
+        $tasks = Task::where('folder_id', $current_folder->id)->get();     // get() を忘れないように
 
         return view('tasks/index', [
             'folders' => $folders,
             'current_folder_id' => $id,
+            'tasks' => $tasks,
         ]);
 
     }
